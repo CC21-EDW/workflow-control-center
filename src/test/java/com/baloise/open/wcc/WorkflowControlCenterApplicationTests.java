810586e5a141a63d698c4a2540df612938462982
tiliavir <tiliavir@users.noreply.github.com>
2021-10-22 23:47:38 +0200

    Initial creation of new spring boot / gradle project with IntelliJ Idea

---
+++ src/test/java/com/baloise/open/wcc/WorkflowControlCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baloise.open.wcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowControlCenterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
