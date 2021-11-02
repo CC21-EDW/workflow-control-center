5a77942094640041674ea8a6e11af0ea299fed2b
tiliavir <tiliavir@users.noreply.github.com>
2021-11-02 18:01:27 +0100

    #12: Fixing coverage

--- cc-api/src/test/java/com/baloise/open/wcc/WorkflowControlCenterApplicationTests.java
+++ cc-api/src/test/java/com/baloise/open/wcc/WorkflowControlCenterApplicationTests.java
@@ -1,13 +1,18 @@
 package com.baloise.open.wcc;
 
+import com.baloise.open.wcc.dto.DateDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class WorkflowControlCenterApplicationTests {
-
   @Test
-  void contextLoads() {
+  void getCurrentServerTime() {
+    TestController tc = new TestController();
+    DateDto currentServerTime = tc.getCurrentServerTime();
+    assertNotNull(currentServerTime);
+    assertNotNull(currentServerTime.getDate());
   }
-
 }
